c87b6bcae5459b53376b153ee1fbbeff0cec4aa6
La maquina <105044865+PavelAKM@users.noreply.github.com>
2022-06-09 09:37:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -238,10 +238,10 @@ def onmessage(update,bot:ObigramClient):
     try:
         thread = bot.this_thread
         username = update.message.sender.username
-        tl_admin_user = os.environ.get('tl_admin_user')
+        tl_admin_user = os.environ.get('PavelAKM')
 
         #set in debug
-        tl_admin_user = os.environ.get('administrador')
+        tl_admin_user = os.environ.get('PavelAKM')
 
         jdb = JsonDatabase('database')
         jdb.check_create()
