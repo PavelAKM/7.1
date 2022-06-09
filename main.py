d2e43d90c09ab9460ce86b3d7550fd48257a493e
La maquina <105044865+PavelAKM@users.noreply.github.com>
2022-06-09 09:25:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -540,7 +540,7 @@ def onmessage(update,bot:ObigramClient):
                 # set in debug
             #    api_id = 7386053
             #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+            #    bot_token = '5368525823:AAGY4PLJIAWtxTr_3YiSK1qicl1BY71745A'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
